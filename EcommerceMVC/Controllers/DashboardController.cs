f13b519b44692f91ccab2e8c148bddbbc9774531
Hnin Htet Zaw <hninhtet.zaw@kbzbank.com>
2025-11-08 16:26:10 +0630

    layout change

---
+++ EcommerceMVC/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMVC.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [HttpGet]
+        public IActionResult UserDashboard()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+    }
+}
